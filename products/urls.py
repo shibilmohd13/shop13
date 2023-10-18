4eb20a33d7e63e56e4efad4e16aab3cdce16bc2c
shibilmohd13 <shibilmohd130@gmail.com>
2023-10-19 01:24:53 +0530

    admin side Read Complete

--- products/urls.py
+++ products/urls.py
@@ -4,8 +4,13 @@ from . import views
 
 urlpatterns = [
     path('',views.products, name='products'),
-    # path('categories', views.categories, name='categories'),
-    # path('brands', views.brands, name='brands'),
-    # path('colors', views.colors, name='colors'),
+    path('product_status/<str:id>',views.product_status, name='product_status'),
+    path('categories', views.categories, name='categories'),
+    path('category_status/<str:id>', views.category_status, name='category_status'),
+    path('brands', views.brands, name='brands'),
+    path('brand_status/<str:id>', views.brand_status, name='brand_status'),
+    path('colors', views.colors, name='colors'),
+    path('color_status/<str:id>', views.color_status, name='color_status'),
+
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,6 +1,75 @@
 from django.shortcuts import render,redirect
+from .models import *
 
 # Create your views here.
 
 def products(request):
-    return render(request, 'admin_panel/products.html')
+    prod = Product.objects.all().order_by('id')
+    context = {
+        'products' : prod
+    }
+    return render(request, 'admin_panel/products.html', context)
+
+def product_status(request, id):
+    prod = Product.objects.filter(id=id).first()
+    if prod.is_listed == True:
+        prod.is_listed = False
+        prod.save()
+    else:
+        prod.is_listed = True
+        prod.save()
+    return redirect('products')
+
+def categories(request):
+    cat = Category.objects.all().order_by('id')
+    context = {
+        'categories' : cat
+    }
+    return render(request, 'admin_panel/categories.html', context)
+
+def category_status(request, id):
+    cat = Category.objects.filter(id=id).first()
+    if cat.is_listed == True:
+        cat.is_listed = False
+        cat.save()
+    else:
+        cat.is_listed = True
+        cat.save()
+    return redirect('categories')
+
+
+def brands(request):
+    brand = Brand.objects.all().order_by('id')
+    context = {
+        'brands' : brand
+    }
+    return render(request, 'admin_panel/brands.html', context)
+
+def brand_status(request, id):
+    brand = Brand.objects.filter(id=id).first()
+    if brand.is_listed == True:
+        brand.is_listed = False
+        brand.save()
+    else:
+        brand.is_listed = True
+        brand.save()
+    return redirect('brands')
+
+
+
+def colors(request):
+    color = Color.objects.all().order_by('id')
+    context = {
+        'colors' : color
+    }
+    return render(request, 'admin_panel/colors.html', context)
+
+def color_status(request, id):
+    color = Color.objects.filter(id=id).first()
+    if color.is_listed == True:
+        color.is_listed = False
+        color.save()
+    else:
+        color.is_listed = True
+        color.save()
+    return redirect('colors')
