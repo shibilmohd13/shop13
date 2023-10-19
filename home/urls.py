6f1d307d85ba3a2f9c101a7011e8bada78f1b131
shibilmohd13 <shibilmohd130@gmail.com>
2023-10-19 20:46:10 +0530

    user products listed

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('',views.home, name='home'),
     path('shop/',views.shop, name='shop'),
-    path('product_details/',views.product_details, name='product_details'),
+    path('shop/product_details/<str:id>',views.product_details, name='product_details'),
     path('logout/',views.logout, name='logout'),
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,22 +1,27 @@
 from django.shortcuts import render,redirect
+from products.models import *
 
 # Create your views here.
 def home(request):
+    obj = Product.objects.all()[:8]
     try: 
         user = request.session['fullname']
-        return render(request, 'home/home.html', {'user' : user})
+        return render(request, 'home/home.html', {'user' : user, 'obj': obj})
     except:
-        return render(request, 'home/home.html', {'user' : 'Login Now'})
+        return render(request, 'home/home.html', {'user' : 'Login Now', 'obj': obj})
 
 def shop(request):
+    obj = Product.objects.all()
     try: 
         user = request.session['fullname']
-        return render(request, 'home/shop.html', {'user' : user})
+        return render(request, 'home/shop.html', {'user' : user, 'obj': obj})
     except:
-        return render(request, 'home/shop.html', {'user' : 'Login Now'})
+        return render(request, 'home/shop.html', {'user' : 'Login Now', 'obj': obj})
 
-def product_details(request):
-    return render(request, "home/details.html")
+def product_details(request,id):
+    obj = Product.objects.filter(id=id)[0]
+    context = {'item' : obj}
+    return render(request, "home/details.html",context)
 
 def logout(request):
     request.session.flush()
